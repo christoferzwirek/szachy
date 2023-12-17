d9670a24e91910ba7c8da5be99833eab22b9650b
Radosław Wirkus <89786927+christoferzwirek@users.noreply.github.com>
2023-12-17 22:31:52 +0100

    Add files via upload

--- szachy.py
+++ szachy.py
@@ -46,91 +46,94 @@ client = berserk.Client(session=session)
 
 #profiler
 def is_move_in_opening_book(board, move, opening_book_path):
-    start2=time.time()
+    #start2=time.time()
     with chess.polyglot.open_reader(opening_book_path) as reader:
         for entry in reader.find_all(board):
             if entry.move == move:
-                print(entry)
+                #print(entry)
                 
                 return True
     
     return False
 
 
-
-# Ścieżka do pliku wykonywalnego Stockfisha
-stockfish_path = "stockfish/stockfish-windows-x86-64.exe"
-opening_book_path = "komodo.bin"
-# Utwórz obiekt silnika szachowego
-engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
-#board2 = game.board()
-
-
-user=input("podaj nazwę użytkownika ").lower()#AinsOowl
-#ile_gier=input("podaj ile gier ")
-start=time.time()
-games=client.games.export_by_player(user,max=100,perf_type='rapid')#
-games1 = list(games)
-result=[]
-for i in range(len(games1)):
+def checkmiss(user):
+    # Ścieżka do pliku wykonywalnego Stockfisha
+    stockfish_path = "stockfish/stockfish-windows-x86-64.exe"
+    opening_book_path = "komodo.bin"
+    # Utwórz obiekt silnika szachowego
+    engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+    #board2 = game.board()
     
-    game_id = games1[i]['id']
-    pgn=client.games.export(game_id,as_pgn=True) 
     
-    # Pozycja, którą chcesz zanalizować
-    game= chess.pgn.read_game(io.StringIO(pgn))
-    headers = {key: value.lower() for key, value in game.headers.items()}
-    
-    preocena=0
-    ocena =0
-    
-    # Inicjalizuj planszę
-    board = game.board()
-    print("\n")
-    # Iteruj przez ruchy gry i analizuj każdą pozycję
-    for ply,move in enumerate(game.mainline_moves()):
+    games=client.games.export_by_player(user,max=10,perf_type='rapid')#
+    games1 = list(games)
+    result=[]
+    for i in range(len(games1)):
+        game_id = games1[i]['id']
+        pgn=client.games.export(game_id,as_pgn=True) 
         
+        # Pozycja, którą chcesz zanalizować
+        game= chess.pgn.read_game(io.StringIO(pgn))
+        headers = {key: value.lower() for key, value in game.headers.items()}
         
-        if is_move_in_opening_book(board, move, opening_book_path):
-            #print(f"{move} is in opeingn book")
-            board.push(move)
-        else:
-            board.push(move)
-            if ply ==20:#kończenie debiutu
-                break
-            analysis = engine.analyse(board, chess.engine.Limit(time=0.5)) # Limit czasowy analizy (np. 0.1 sekundy)
-            #result.append(analysis.score)
-            # Wyświetl lub zapisz wyniki analizy dla każdego ruchu
-            if headers['White'] == user:
-                info =analysis['score']
-                ocena=info.white()
-            elif headers['Black'] == user:
-                info =analysis['score']
-                ocena=info.black()
+        preocena=0
+        ocena =0
+        
+        # Inicjalizuj planszę
+        board = game.board()
+        print("\n")
+        # Iteruj przez ruchy gry i analizuj każdą pozycję
+        for ply,move in enumerate(game.mainline_moves()):
+            
+            
+            if is_move_in_opening_book(board, move, opening_book_path):
+                #print(f"{move} is in opeingn book")
+                board.push(move)
             else:
-                print(f"{user} is not part of this game or their color is not specified in PGN headers.")
-            if preocena-ocena.cp>200:
-                #print(f"ocena {ocena.cp},pporzednia ocena {preocena}")
-                #print(preocena-ocena.cp)
-                #print(f"Move: {move}, Evaluation: {analysis['score']}")     
-                result.append((board.fen(),move))
-            preocena=ocena.cp
+                board.push(move)
+                if ply ==20:#kończenie debiutu
+                    break
+                analysis = engine.analyse(board, chess.engine.Limit(time=0.5)) # Limit czasowy analizy (np. 0.1 sekundy)
+                #result.append(analysis.score)
+                # Wyświetl lub zapisz wyniki analizy dla każdego ruchu
+                if headers['White'] == user:
+                    info =analysis['score']
+                    ocena=info.white()
+                elif headers['Black'] == user:
+                    info =analysis['score']
+                    ocena=info.black()
+                else:
+                    print(f"{user} is not part of this game or their color is not specified in PGN headers.")
+                if preocena-ocena.cp>200:
+                    #print(f"ocena {ocena.cp},pporzednia ocena {preocena}")
+                    #print(preocena-ocena.cp)
+                    #print(f"Move: {move}, Evaluation: {analysis['score']}")     
+                    result.append((board.fen(),move))
+                preocena=ocena.cp
+        
+        #best_move = info.get("pv", [])[0]
+        #print("Najlepszy ruch:", best_move)
+        #print("Ocena pozycji:", info["score"])
+        
+        # Zamknij silnik szachowy po zakończeniu
+    engine.quit()
+    return result
+
+if __name__ == '__main__':
     
-    #best_move = info.get("pv", [])[0]
-    #print("Najlepszy ruch:", best_move)
-    #print("Ocena pozycji:", info["score"])
-    
-    # Zamknij silnik szachowy po zakończeniu
-
-
-engine.quit()
-stop=time.time()
-executio= stop - start
-
-# Wyświetl czas wykonania
-print(f"Czas wykonania programu: {executio:.2f} sekundy")
-
-"""
-for fen,m in result:
-    print(m) 
-"""
+    user=input("podaj nazwę użytkownika ").lower()#AinsOowl
+    #ile_gier=input("podaj ile gier ")
+    start=time.time()
+    checkmiss(user)
+    stop=time.time()
+    executio= stop - start
+ 
+    # Wyświetl czas wykonania
+    print(f"Czas wykonania programu: {executio:.2f} sekundy")
+ 
+    """
+    for fen,m in result:
+        print(m) 
+    """
+   
